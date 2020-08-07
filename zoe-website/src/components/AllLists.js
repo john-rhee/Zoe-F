c1ce31e610bc97e6976050c4a2d711f46629839b
Johnnyr81 <emailjohn81@gmail.com>
2020-08-07 03:37:25 -0500

    update

--- zoe-website/src/components/AllLists.js
+++ zoe-website/src/components/AllLists.js
@@ -1,8 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import axios from "axios";
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import SingleList from './SingleList';
 import WunderContext from '../contexts/WunderContext';
+import dPicture from '../images/defaultImage.png';
 
 const initialItem = {
 
@@ -10,16 +12,43 @@ const initialItem = {
     
 };
 
-
 function AllLists() {
 
     const {mainForm, setMainForm} = useContext(WunderContext);
+    //uploaded picture//
+    const [uFile, setUFile] = useState("none");
+    const [url, setUrl] = useState(dPicture);
+
+    //picture//
+    const [selectedFile, setSelectedFile] = useState(null);
+    
+    //picture//
+    const fileSelectHandler = event => {
+        setSelectedFile(event.target.files[0])
+    }
+    //picture//
+    const fileUploadHandler = event => {
+        console.log("upload started")
+        const fd = new FormData() 
+        fd.append("uimage", selectedFile, selectedFile.name)
+        axios
+            .post('https://zoe-backend.herokuapp.com/upload', fd)
+            .then(response => {
+                console.log(response);
+                setUFile(response.data.picture.name)
+                setUrl(response.data.url)
+               
+            })
+    }
         
     return (
 
         <div>
             <div>
             <h2>Welcome to Zoe's World</h2>
+            <h3>Picture:{uFile}</h3>
+        
+            <img src={url}/>
 
             <Link to="/search">
             <button>Search Tasks</button>
@@ -35,7 +64,13 @@ function AllLists() {
                     <Route render={props => {return <SingleList {...props} wList={todo} />}} />
                 </div>
             ))}
+            </div>
 
+            {/* picture */}
+            <div>
+            <h3>Upload picture</h3>
+            <input type="file" onChange={fileSelectHandler}/>
+            <button onClick={fileUploadHandler}>Upload</button>
             </div>
 
         </div>
